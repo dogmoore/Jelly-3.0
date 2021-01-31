214908e9ba68becf2c5e28539747e87cc4c35a58
dogmoore <dogmoore024@gmail.com>
2021-01-31 02:57:28 -0600

    added shutdown and test command and attempted listeners

--- Cogs/Events.py
+++ Cogs/Events.py
@@ -1,5 +1,6 @@
 import discord
 import yaml
+import random
 from discord.ext import commands
 
 with open("config.yml", "r") as ymlfile:
@@ -13,6 +14,39 @@ class Events(commands.Cog):
     def __init__(self, client):
         self.client = client
         #self._cd = commands.CooldownMapping.from_cooldown(1.0, 10.0, commands.BucketType.guild)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        client = self.client
+        try:
+            oneInHundred = random.randint(1, 100)
+            oneInTen = random.randint(1, 10)
+
+            if message.author.id == 278548721778688010 and oneInHundred == 7:
+                await message.reply('DON\'T LISTEN TO THIS HEATHEN, I AM THE REAL JELLY!')
+            elif 'Who do you love jelly 3.0'.lower() in message.content.lower():
+                if oneInTen == 1:
+                    await message.send('some gay fuck')
+                elif oneInTen == 2:
+                    await message.send('DOGMOORE!')
+                elif oneInTen == 3:
+                    await message.send('some weird chick who says she\'s a cat')
+                elif oneInTen == 4:
+                    await message.send('a ginger hehe')
+                elif oneInTen == 5:
+                    await message.send('APaperbot')
+                elif oneInTen == 6:
+                    await message.send('Jellyfish!...but he never responds to my texts....D:')
+                elif oneInTen == 7:
+                    await message.send('PAPEE')
+                elif oneInTen == 8:
+                    message.send('FATAL!!!!!!')
+                elif oneInTen == 9:
+                    await message.send('idk, no one?')
+                elif oneInTen == 10:
+                    await message.send('The one and only Queen, Freddie Mercury!')
+        except:
+            pass
 
 def setup(client):
     client.add_cog(Events(client))
--- Cogs/general.py
+++ Cogs/general.py
@@ -1,9 +1,15 @@
 import yaml
 import discord
+import random
 from discord.ext import commands
+# from abc import ABCMeta
+# from abc_delegation import delegation_metaclass
 
 with open("config.yml", "r") as ymlfile:
     config = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+# class message:
+#     discord.abc.Snowflake
 
 # with open("bot.yml", "r") as ymlfile:
 #     bot = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -12,10 +18,22 @@ class Utils(commands.Cog):
     def __init__(self,client):
         self.client = client
 
-@commands.command(name='ping')
-async def ping(self, context):
-    client = self.client
-    await message.send('test')
+    @commands.command(name='ping')
+    async def ping(self, context):
+        client = self.client
+        await context.send('test')
+
+
+
+    @commands.command(name='shutdown')
+    async def shutdown(self, context):
+        client = self.client
+        if context.author.id == 376857933067321366 or context.author.id == 278548721778688010:
+            await context.send('Ok, time for bed')
+            await context.send('Dog and Jelly 1.0 let\'s go sleep')
+            await client.close()
+        else:
+            await context.send('Something went wrong')
 
 def setup(client):
    client.add_cog(Utils(client))
